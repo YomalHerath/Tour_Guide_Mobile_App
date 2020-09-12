4c4a219a68e4fba5badbbde56749d0d2825aad74
Yomal Herath <yomalherath1121@gmail.com>
2020-09-12 14:09:55 +0530

    add Splash Screen for Mobile Application

--- app/src/main/java/com/example/tour_guide/Common/SplashScreen.java
+++ app/src/main/java/com/example/tour_guide/Common/SplashScreen.java
@@ -2,17 +2,55 @@ package com.example.tour_guide.Common;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.tour_guide.R;
+import com.example.tour_guide.User.UserDashboard;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 5000;
+
+    //variables
+    ImageView backgroundImage;
+    TextView poweredByLine;
+
+    //Animations
+    Animation sideAnim, bottomAnim;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_screen);
+
+        //Hocks
+        backgroundImage = findViewById(R.id.background_image);
+        poweredByLine = findViewById(R.id.powered_by);
+
+        //Animations
+        sideAnim = AnimationUtils.loadAnimation(this, R.anim.side_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
+
+        //Set Animation on Elements
+        backgroundImage.setAnimation(sideAnim);
+        poweredByLine.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, UserDashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME);
     }
 }
---
+++ app/src/main/java/com/example/tour_guide/User/UserDashboard.java
@@ -0,0 +1,15 @@
+package com.example.tour_guide.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tour_guide.R;
+
+public class UserDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+    }
+}
